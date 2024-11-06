5c743a22459f8d8853fb54ba81dc47293957a893
efettf <efettf@proton.me>
2024-11-06 20:54:25 +0100

    :wrench: Add basic configuration

--- default.nix
+++ default.nix
@@ -1,5 +1,24 @@
 {
   config = {
-    test.value = "string";
+    # Enable wi-fi support via network manager.
+    networking.networkmanager.enable = true;
+
+    # Enable flakes and nix command.
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+    # Disable nano, you can get it back using 'nix-shell -p nano'.
+    programs.nano.enable = false;
+
+    security = {
+      # Simple sudo replacement.
+      doas.enable = true;
+
+      # Disable sudo, you can get it back using 'nix-shell -p sudo'.
+      sudo.enable = false;
+
+      # Removes password prompts for your wheel group,
+      # you can manage user groups below at '[users.users.*]'.
+      doas.wheelNeedsPassword = false;
+    };
   };
 }
